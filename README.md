a7a589f1d9b31f4614f6ff9b5bbc5817e7cb7378
lovezsh <shuxhan@163.com>
2023-04-18 09:34:23 +0800

    build readme

--- README.md
+++ README.md
@@ -2,6 +2,6 @@
 
 一个周刊，记录有趣的软件和网站，目前已更新到第 11 期。
 
-网站：[https://weekly.zsh.im/](https://weekly.zsh.im/)
+
 
 欢迎投稿：https://github.com/lovezsh/weekly/issues
---
+++ build.py
@@ -0,0 +1,40 @@
+import os
+import httpx
+import re
+import urllib.parse
+import datetime
+
+def fetch_ci_time(filePath):
+    entries = httpx.get("https://api.github.com/repos/lovezsh/weekly/commits?path=" + filePath + "&page=1&per_page=1")
+    ciTime= entries.json()[0]["commit"]["committer"]["date"].split("T")[0]
+    return ciTime
+    # return datetime.datetime.strptime(ciTime,"%Y-%m-%d")
+
+if __name__ == "__main__":
+  readmefile=open('README.md','w')
+  readmefile.write("## weekly\n\n网站: [https://weekly.zsh.im/](https://weekly.zsh.im/)\n\n> 一个周刊，记录有趣的软件和网站。欢迎 [投稿推荐](https://github.com/lovezsh/weekly/issues/) 你的好东西。")
+
+  for root, dirs, filenames in os.walk('./docs/posts'):
+    filenames = sorted(filenames, key=lambda x:float(re.findall("(\d+)",x)[0]), reverse=True)
+
+  for index, name in enumerate(filenames):
+      if name.endswith('.md'):
+        filepath = urllib.parse.quote(name)
+        oldTitle = name.split('.md')[0]
+        url   = 'https://weekly.zsh.im/posts/' + oldTitle
+        title = '第 ' + oldTitle.split('-')[0] + ' 期 - ' + oldTitle.split('-')[1]
+        readmeMd= '* [{}]({})\n'.format(title, url)
+        dateList = ["2022-10-10","2022-09-26","2022-09-12","2022-09-05","2022-08-29"]
+        num = int(oldTitle.split('-')[0])
+        if index < 5 :
+          if num < 100 :
+            modified = dateList[99-num]
+          else :
+            modified = fetch_ci_time('/docs/posts/' + filepath)
+
+          recentMd= '* [{}]({}) - {}\n'.format(title, url, modified)
+          recentfile.write(recentMd)
+        readmefile.write(readmeMd)
+
+  recentfile.close()
+  readmefile.close()
