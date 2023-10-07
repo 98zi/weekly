7c672330d510b8eb8b086508fb76012efd1c82e5
98zi <anghunk@gmail.com>
2023-10-07 20:44:24 +0800

    :sparkles: 新增 giscus

--- docs/.vitepress/theme/index.js
+++ docs/.vitepress/theme/index.js
@@ -1,17 +1,24 @@
 import DefaultTheme from 'vitepress/theme'
+import giscusTalk from 'vitepress-plugin-comment-with-giscus';
+import { useData, useRoute } from 'vitepress';
+
 import './custom.css'
 
 import Layout from './Layout.vue'
 
-import { onMounted, watch, nextTick } from 'vue' 
-import { useRoute } from 'vitepress'
+import { onMounted, watch, nextTick } from 'vue'
 import mediumZoom from 'medium-zoom'
 
 export default {
   ...DefaultTheme,
   Layout,
+  enhanceApp(ctx) {
+    DefaultTheme.enhanceApp(ctx);
+    // ...
+  },
   setup() {
-    const route = useRoute() 
+    const route = useRoute();
+    const { frontmatter } = useData();
     const initZoom = () => {
       mediumZoom('.main img', { background: 'var(--vp-c-bg)' })
     }
@@ -22,5 +29,20 @@ export default {
       () => route.path,
       () => nextTick(() => initZoom())
     )
+
+    // Obtain configuration from: https://giscus.app/
+    giscusTalk({
+      repo: '98zi/blog',
+      repoId: 'R_kgDOJWODqg',
+      category: 'Announcements',
+      categoryId: 'DIC_kwDOJWODqs4CV8b5',
+      mapping: 'pathname',
+      inputPosition: 'top',
+      lang: 'zh-CN',
+      loading: "lazy",
+      theme: "light_tritanopia",
+    }, {
+      frontmatter, route
+    });
   },
 }
